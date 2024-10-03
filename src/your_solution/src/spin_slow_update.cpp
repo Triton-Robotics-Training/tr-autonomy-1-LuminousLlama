74b27449881b4eaccb6d9865f667ab592eb85991
Luminous_Llama <keyushattarde@gmail.com>
2024-10-02 17:30:21 -0700

    part 1

--- src/your_solution/src/spin_slow_update.cpp
+++ src/your_solution/src/spin_slow_update.cpp
@@ -1,15 +1,29 @@
 #include "spin_slow_update.h"
+using std::placeholders::_1;
 
-int main(int argc, char *argv[]) {
+int main(int argc, char *argv[])
+{
   rclcpp::init(argc, argv);
   rclcpp::spin(std::make_shared<SlowSolution>());
   rclcpp::shutdown();
   return 0;
 }
 
-SlowSolution::SlowSolution() : Node("slowsolution") {
-  RCLCPP_INFO(this->get_logger(), "Remove this statement from spin_slow_update.cpp");
-  // your code here
+
+SlowSolution::SlowSolution() : Node("slowsolution")
+{
+  // RCLCPP_INFO(this->get_logger(), "Remove this statement from spin_slow_update.cpp");
+  mpSubscription_ = this->create_subscription<std_msgs::msg::Float64MultiArray>(
+      "measuredpos",
+      10,
+      std::bind(&SlowSolution::topic_callback, this, _1));
+
+  ppPublisher_ = this->create_publisher<ArrayMsg>("predictedpos", 10);
+}
+
+void SlowSolution::topic_callback(const ArrayMsg::SharedPtr msg){
+  RCLCPP_INFO(this->get_logger(), "received: %f, %f", msg->data[0], msg->data[1]);
+  ppPublisher_->publish(*msg);
 }
 
 // your code here
--- src/your_solution/src/spin_slow_update.h
+++ src/your_solution/src/spin_slow_update.h
@@ -10,6 +10,9 @@ class SlowSolution : public rclcpp::Node {
   SlowSolution();
  private:
   // your code here
+  rclcpp::Publisher<ArrayMsg>::SharedPtr ppPublisher_;
+  rclcpp::Subscription<ArrayMsg>::SharedPtr mpSubscription_;
+  void topic_callback(const ArrayMsg::SharedPtr msg);
 };
 
 #endif //YOUR_SOLUTION_SRC_SPIN_SLOW_UPDATE_H_
